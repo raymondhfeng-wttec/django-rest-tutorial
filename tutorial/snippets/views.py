66934aac7e1e4173d191c5432527f71d6e82363f
raymondhfeng <raymondhfeng@berkeley.edu>
2020-07-10 00:35:11 -0700

    Part 5 Complete

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -25,6 +25,17 @@ from rest_framework import permissions
 
 from snippets.permissions import IsOwnerOrReadOnly
 
+from rest_framework import renderers
+
+from rest_framework.reverse import reverse
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -36,6 +47,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
