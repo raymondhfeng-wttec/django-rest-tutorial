cd1fc45692283f13aa9e358accba936d9586af6f
raymondhfeng <raymondhfeng@berkeley.edu>
2020-07-09 18:21:38 -0700

    Part 4 Finished

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -18,11 +18,29 @@ from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
 
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+from rest_framework import permissions
+
+from snippets.permissions import IsOwnerOrReadOnly
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
